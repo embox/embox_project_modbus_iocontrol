85dd78760fd27275d5d6714ccdc9b9b52ef8619b
Anton Bondarev <anton.bondarev2310@gmail.com>
2021-02-26 12:53:09 +0300

    minor: clean modbus_server.c led-client.c

--- modbus/host_cmds/led-client.c
+++ modbus/host_cmds/led-client.c
@@ -6,29 +6,13 @@
  * @date    06.11.2014
  */
 
-/*
- * Copyright © 2008-2010 Stéphane Raimbault <stephane.raimbault@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+
 #include <modbus.h>
 
 int main(int argc, char *argv[]) {
